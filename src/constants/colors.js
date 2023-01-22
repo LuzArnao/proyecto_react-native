6fd1b9cc199f5b3ccf88593031b856cc808a9053
LuzArnao <104283596+LuzArnao@users.noreply.github.com>
2023-01-22 20:02:45 -0300

    Delete src directory

--- src/constants/colors.js
+++
@@ -1,7 +0,0 @@
-export const COLORS = {
-    primary: '#443F43',
-    secondary: '#FFFFFF',
-    terceary: '#C17BB1',
-    tercearyOP: 'rgba(193, 123, 177, 0.7)',
-    whiteOP: 'rgba(255, 255, 255, 0.85)',
-};
--- src/constants/img.js
+++
@@ -1,1 +0,0 @@
-export const backgroundPrincipal = require('../assets/img/background.jpeg')
